350743f97fd432f9184ff79a846a24854132ca2a
danielbohnrs <dani_bohn_rs@hotmail.com>
2021-05-05 17:33:39 -0300

    postgreSQL added

--- backend/create.sql
+++ backend/create.sql
@@ -1,3 +1,3 @@
-create table tb_sales (id bigint not null auto_increment, amount double precision, date date, deals integer, visited integer, seller_id bigint, primary key (id)) type=MyISAM;
-create table tb_sellers (id bigint not null auto_increment, name varchar(255), primary key (id)) type=MyISAM;
-alter table tb_sales add constraint FKp0nn0oixeiw2fq3jivj6vnsu foreign key (seller_id) references tb_sellers (id);
+create table tb_sales (id  bigserial not null, amount float8, date date, deals int4, visited int4, seller_id int8, primary key (id));
+create table tb_sellers (id  bigserial not null, name varchar(255), primary key (id));
+alter table tb_sales add constraint FKp0nn0oixeiw2fq3jivj6vnsu foreign key (seller_id) references tb_sellers;
